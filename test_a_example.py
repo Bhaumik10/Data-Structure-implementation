01d0513cfbca8488dbbac21e72c154c7d9999cc4
Bhaumik10 <bp1232@nyu.edu>
2016-04-10 20:24:42 -0400

    test example in python

---
+++ test_a_example.py
@@ -0,0 +1,35 @@
+arr = []
+for arr_i in xrange(6):
+    arr_temp = map(int,raw_input().strip().split(' '))
+    arr.append(arr_temp)
+
+sum = 0
+a = []
+
+
+for i in xrange(4):
+    for j in xrange(5):
+        new_sum = arr[j][i] + arr[j][i+1] + arr[j][i+2]
+
+        if j < 5:
+            j = j + 1
+        if i < 5 :
+            i = i + 1
+        new_sum = new_sum + arr[j][i]
+
+        if j < 5:
+            j = j + 1
+        if i < 5:
+            i = i - 1
+        new_sum = new_sum + arr[j][i] + arr[j][i+1] + arr[j][i+2]
+
+        if new_sum > sum:
+            sum = new_sum
+            a.append(sum)
+        else:
+            ''
+        if i == 5:
+            break
+
+print a
+print max(a)
