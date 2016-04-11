700a0bfeede55a6b3e33a6c435144b1cad92f15c
Bhaumik10 <bp1232@nyu.edu>
2016-04-10 20:30:20 -0400

    linkedlist implementation in python

---
+++ linkedlist.py
@@ -0,0 +1,72 @@
+class Node(object):
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+
+class LinkedList(object):
+    def __init__(self, head=None):              #it is constructor
+        self.head = head
+
+    def insert(self, data):
+        new_node = Node(data)
+        new_node.set_next(self.head)
+        self.head = new_node            # Here it sets head to the new Node
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current:
+            count += 1
+            current = current.get_next()
+        return count
+
+    def search(self, data):
+        current = self.head
+        found = False
+        while current and found is False:
+            if current.get_data() == data:
+                found = True
+            else:
+                current = current.get_next()
+        if current is None:
+            raise ValueError("Data not in list")
+        return current
+
+    def delete(self, data):
+        current = self.head
+        previous = None
+        found = False
+        while current and found is False:
+            if current.get_data() == data:
+                found = True
+            else:
+                previous = current
+                current = current.get_next()
+        if current is None:
+            raise ValueError("Data not in list")
+        if previous is None:
+            self.head = current.get_next()
+        else:
+            previous.set_next(current.get_next())
+
+
+k = LinkedList()
+#
+# for i in range(1,10):
+#     n = int(raw_input())
+#     k.insert(n)
+
+k.insert(5)
+
+#print k.search(5)
+
